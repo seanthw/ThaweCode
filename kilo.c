79a3ef2d2ad58cd416943bdc59b0fd0fadb09d35
sean <sean.thawe@outlook.com>
2025-09-06 17:06:40 +0200

    refactor(ui): horizontally center the welcome message

--- kilo.c
+++ kilo.c
@@ -128,8 +128,14 @@ void editorDrawRows(struct abuf *ab) {
       int welcomelen = snprintf(welcome, sizeof(welcome),
                                 "kilo editor --version %s", KILO_VERSION);
       if (welcomelen > E.screencols) welcomelen = E.screencols;
+      int padding = (E.screencols - welcomelen) / 2;
+      if(padding) {
+        abAppend(ab, "~", 1);
+        padding--;
+      }
+      while (padding--) abAppend(ab, " ", 1);
       abAppend(ab, welcome, welcomelen);
-    } else{
+    } else {
       abAppend(ab, "~", 1);
     }
 
