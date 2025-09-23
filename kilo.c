0e97d07be29f891cd76868d1fbadc9818c083053
sean <sean.thawe@outlook.com>
2025-09-23 16:08:21 +0200

    feat: add increment dirty to have a sense of how dirty a file is

--- kilo.c
+++ kilo.c
@@ -228,6 +228,7 @@ void editorAppendRow(char *s, size_t len) {
   E.row[at].render = NULL;
   editorUpdateRow(&E.row[at]);
   E.numrows++;
+  E.dirty++;
 }
 
 void editorRowInsertChar(erow *row, int at, int c) {
@@ -237,6 +238,7 @@ void editorRowInsertChar(erow *row, int at, int c) {
   row->size++;
   row->chars[at] = c;
   editorUpdateRow(row);
+  E.dirty++;
 }
 
 /*** editor operations ***/
