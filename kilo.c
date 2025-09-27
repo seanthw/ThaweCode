627ecefbc20cb0b0b979fa909ed35551dd9022ae
sean <sean.thawe@outlook.com>
2025-09-27 15:12:19 +0200

    feat: map Ctrl-f to editorFind() function with add it to the help message

--- kilo.c
+++ kilo.c
@@ -673,6 +673,10 @@ void editorProcessKeypress() {
         E.cx = E.row[E.cy].size;
       break;
 
+    case CTRL_KEY('f'):
+      editorFind();
+      break;
+
     case BACKSPACE:
     case CTRL_KEY('h'):
     case DEL_KEY:
@@ -741,7 +745,8 @@ int main(int argc, char *argv[]) {
     editorOpen(argv[1]);
   }
 
-  editorSetStatusMessage("HELP: Ctrl-S = save | Ctrl-Q = quit");
+  editorSetStatusMessage(
+    "HELP: Ctrl-S = save | Ctrl-Q = quit | Ctrl-F = find");
 
   while (1) {
     editorRefreshScreen();
