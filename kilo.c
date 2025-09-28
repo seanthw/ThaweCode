9f76d590bf6d7226705a901df49aff04433f49ee
sean <sean.thawe@outlook.com>
2025-09-28 02:07:55 +0200

    fix: got some values wrong for the editorupdatesyntax which was avoiding digits from displaying red

--- kilo.c
+++ kilo.c
@@ -192,13 +192,13 @@ int getWindowSize(int *rows, int *cols) {
 /*** syntax highliting ***/
 
 void editorUpdateSyntax(erow *row) {
-  row->hl = realloc(row->hl, row->size);
-  memset(row->hl, HL_NORMAL, row->size);
+  row->hl = realloc(row->hl, row->rsize);
+  memset(row->hl, HL_NORMAL, row->rsize);
 
   int i;
-  for(i = 0; i <row->size; i++){
+  for(i = 0; i <row->rsize; i++){
     if (isdigit(row->render[i])) {
-      row->hl[i] = HL_NORMAL;
+      row->hl[i] = HL_NUMBER;
     }
   }
 }
