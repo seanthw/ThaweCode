af6de755667de869ec139dd0204d8be6e9106078
Sean Thawe <sean.thawe@outlook.com>
2025-10-23 02:15:42 +0200

    feat: add syntax highlighting for javascript

--- kilo.c
+++ kilo.c
@@ -116,6 +116,24 @@ char *PY_HL_keywords[] = {
   NULL
 };
 
+char *JS_HL_extensions[] = {".js", NULL};
+char *JS_HL_keywords[] = {
+  /* Standard Keywords */
+  "break", "case", "catch", "class", "const", "continue", "debugger",
+  "default", "delete", "do", "else", "export", "extends", "finally",
+  "for", "function", "if", "import", "in", "instanceof", "new",
+  "return", "super", "switch", "this", "throw", "try", "typeof", "var",
+  "void", "while", "with", "yield",
+
+  /* Contextual/Future-Proof Keywords with secondary highlighting */
+  "let|", "static|", "enum|", "await|", "implements|", "package|",
+  "protected|", "interface|", "private|", "public|",
+
+  /* Literals with secondary highlighting */
+  "true|", "false|", "null|",
+  NULL
+};
+
 struct editorSyntax HLDB[] = {
   {
     "c",
@@ -129,6 +147,13 @@ struct editorSyntax HLDB[] = {
     PY_HL_extensions,
     PY_HL_keywords,
     "#", NULL, NULL,
+    HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS
+  },
+  {
+    "javascript",
+    JS_HL_extensions,
+    JS_HL_keywords,
+    "//", "/*", "*/",
     HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS
   }
 };
